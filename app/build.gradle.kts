4a3c8d36c29bda76ba1751b85f48ee7cdc516e70
Vishal Gupta <vshal9921@gmail.com>
2025-04-14 10:53:29 +0530

    versionName updated tp 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("debug")
