9e2247a6844ff5b60b982a8ef35903d8eed480fe
Vishal Gupta <vshal9921@gmail.com>
2025-04-14 10:48:46 +0530

    versionCode updated to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.vshal9921.realtimescan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
