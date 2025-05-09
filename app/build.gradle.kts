ea751ff7e3ffd25924edcd26f11d3920d1a245c8
Vishal Gupta <vshal9921@gmail.com>
2025-05-09 16:41:18 +0530

    version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.vshal9921.realtimescan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("debug")
