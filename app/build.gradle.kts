469bcbf6c7b5901028f498babed7bff819a6ab38
Vishal Gupta <vshal9921@gmail.com>
2025-05-09 16:55:38 +0530

    fail pipeline + version increase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.vshal9921.realtimescan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("debug")
